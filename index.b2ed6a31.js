b202c8285ca85e5f6b529d7c4bd8e64ebb7c2c47
Mari Andrushko <kap.mebel.a@gmail.com>
2024-07-17 15:01:18 +0200

    make build

---
+++ index.b2ed6a31.js
@@ -0,0 +1,2 @@
+var t,e,l,c,i,o;t=document.querySelector(".wall"),e=document.querySelector(".spider"),l=t.clientWidth,c=t.clientHeight,i=e.clientWidth,o=e.clientHeight,e.style.position="absolute",e.style.left="".concat((c-o)/2,"px"),e.style.top="".concat((l-i)/2,"px");
+//# sourceMappingURL=index.b2ed6a31.js.map
